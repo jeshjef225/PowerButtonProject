61730d04adc3382759d7bfeb3a5fddaf5210f143
jeshjef225 <52910753+jeshjef225@users.noreply.github.com>
2021-05-17 20:26:45 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,11 @@
+function green_btn(){
+    document.body.style.background="green"
+}
+
+function yellow_btn(){
+    document.body.style.background="yellow"
+}
+
+function red_btn(){
+    document.body.style.background="red"
+}
